149b9d61dd6cbdf011ba1227504c9a7a36687eaa
Manoj5381 <manojmanojmk2004@gmail.com>
2026-09-07 11:29:21 +0530

    Display the repository cloning status

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // My placements Goal is 
-        System.out.println("--- My Placement goal");
-        System.out.println("Role: Software Developer");
-        System.out.println("Target:Get placed");
-        System.out.println("Daily Practice:8 hours");
-    }}
-
