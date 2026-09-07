fa6f272d3b1e58fceb1e7141c4c024741a671d13
Manoj5381 <manojmanojmk2004@gmail.com>
2026-09-07 11:59:02 +0530

    Student Id Card

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/StudentIdCard.java
@@ -0,0 +1,10 @@
+class StudentIdCard {
+    public static void main(String[] args) {
+        System.out.println("----------------");
+        System.out.println("Student Profile");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level:Beginner");
+        System.out.println("----------------");
+    }
+}
